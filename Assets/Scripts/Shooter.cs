1aab586b4a3baa5d7eb02ef780855c5590d4184c
danijel <danijel.husakovic@gmail.com>
2018-05-08 17:15:52 +0200

    Defenders animations are now controlled by bools and triggers. Important: separated projectile from defender GameObject, so they live if defender dies. Added Gun to position newly spawned projectiles. They spawn at organizing superobject Projectiles. Destroyed by shredder. New specialization of defender: Shooter. They fire on animator's attack curve at moment of animation key calls the Fire function.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile;
+	public GameObject projectileParent;
+	public GameObject gun;
+
+	private void Fire(){
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.parent = projectileParent.transform;
+		newProjectile.transform.position = gun.transform.position;
+	}
+
+}
